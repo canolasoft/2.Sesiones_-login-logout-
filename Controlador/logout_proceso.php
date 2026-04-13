39afaeec1a2571323b5d8d05c7eded9ae4e56533
Fernando Walpert <69522455+canolasoft@users.noreply.github.com>
2026-04-13 10:35:02 -0300

    Implement session logout process

--- Controlador/logout_proceso.php
+++ Controlador/logout_proceso.php
@@ -1,6 +1,25 @@
 <?php
+    // 1. Start the session
     session_start();
+    
+    // 2. Unset all session variables
     $_SESSION = array();
+    
+    // 3. Delete the session cookie
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(
+            session_name(), 
+            '', 
+            time() - 42000, // Expiration time in the past
+            $params["path"], 
+            $params["domain"], 
+            $params["secure"], 
+            $params["httponly"]
+        );
+    }
+    
+    // 4. Destroy the session on the server
     session_destroy();
     header('location: ../Vista/login.php')
 ?>
